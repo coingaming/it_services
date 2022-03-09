7fee716760c8b42c6493147c7583316fa66110dc
boleque <vlkislitsyn1090@gmail.com>
2022-03-09 22:12:56 +0200

    Added terraform to build aws infrastructure (ec2, db)

---
+++ deploy/cmdbuild/terraform-aws-infrastructure/environments/live/providers.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+  backend "s3" {
+    bucket = "tfstate-41c0wnz5vk5e"
+    key    = "cg-live-iam"
+    region = "eu-central-1"
+  }
+
+  required_version = "~> 1.0"
+}
+
+provider "aws" {
+  region = "eu-central-1"
+}
