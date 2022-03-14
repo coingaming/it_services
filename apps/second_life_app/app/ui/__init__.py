87ed7b681b83cc913c6891928fb9824ba26005f7
boleque <vlkislitsyn1090@gmail.com>
2022-03-14 16:05:18 +0200

    Cash payments only description removed

--- apps/second_life_app/app/ui/__init__.py
+++ apps/second_life_app/app/ui/__init__.py
@@ -351,7 +351,7 @@ def get_item_ad_blocks(
 		"elements": [
 			{
 				"type": "plain_text",
-				"text": "Cash payments only. Returns are not accepted",
+				"text": "Returns are not accepted",
 			}
 		],
 		"block_id": BLOCK_IDS.AD_SALE_CONDITION
