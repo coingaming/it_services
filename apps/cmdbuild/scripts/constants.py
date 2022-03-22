01ed7aae47badfa10a49f4e04fdd52547d7bbd0c
boleque <vlkislitsyn1090@gmail.com>
2022-03-22 16:50:08 +0200

    Domain model and schema added

--- apps/cmdbuild/scripts/constants.py
+++ apps/cmdbuild/scripts/constants.py
@@ -11,33 +11,33 @@ class CascadeActionTypes:
     DO_NOT_DELETE_IF_HAS_RELATIONS: str = "restrict"
     DELETE_RELATION: str = "setnull"
     DELETE_ALSO_RELATED_CARDS: str = "delete"
-    RANGE = (DO_NOT_DELETE_IF_HAS_RELATIONS, DELETE_RELATION, DELETE_ALSO_RELATED_CARDS)
+    RANGE: tuple = (DO_NOT_DELETE_IF_HAS_RELATIONS, DELETE_RELATION, DELETE_ALSO_RELATED_CARDS)
 
 
 class ClassType:
     STANDART: str = "standard"
     SIMPLE: str = "simple"
-    RANGE = (STANDART, SIMPLE)
+    RANGE: tuple = (STANDART, SIMPLE)
 
 
 class ClassAttributeType:
-    BIGINTEGER = "long" # todo write "Unit of measure"
-    BOOLEAN = "boolean"
-    CHAR = "char"
-    DATE = "date"
-    DECIMAL = "decimal" # todo choose "Precision Scale" lookup
-    DOUBLE = "double" # todo write "Unit of measure"
-    FILE = "file" # todo choose "DMS category" [Document, Image] lookup
-    FORMULA = "formula" # todo choose "Function type" [Function, Script] lookup
-    INTEGER = "integer" # todo write "Unit of measure"
-    IP_ADDRESS = "ipAddress" # todo choose "Choose ip type" lookup
-    LINK = "link" # todo set bool "Show label"
-    LOOKUP = "lookup" # todo set link to lookup class 
-    LOOKUPARRAY = "lookupArray" # todo set lookup link
-    REFERENCE = "reference" # todo set domain link 
-    STRING = "string" # todo choose "Editor type" [default, password] lookup, "Max Length"
-    TEXT = "text" # todo choose "Type" [Plain text, Editor HTML, Editor MARKDOWN] lookup
-    TIME = "time" # todo set bool "Show second"
-    TIMESTAMP = "dateTime" # todo set bool "Show second"
-    RANGE = (BIGINTEGER, BOOLEAN, CHAR, DATE, DECIMAL, DOUBLE, FILE, FORMULA, INTEGER, IP_ADDRESS, \
+    BIGINTEGER: str = "long" # todo write "Unit of measure"
+    BOOLEAN: str = "boolean"
+    CHAR: str = "char"
+    DATE: str = "date"
+    DECIMAL: str = "decimal" # todo choose "Precision Scale" lookup
+    DOUBLE: str = "double" # todo write "Unit of measure"
+    FILE: str = "file" # todo choose "DMS category" [Document, Image] lookup
+    FORMULA: str = "formula" # todo choose "Function type" [Function, Script] lookup
+    INTEGER: str = "integer" # todo write "Unit of measure"
+    IP_ADDRESS: str = "ipAddress" # todo choose "Choose ip type" lookup
+    LINK: str = "link" # todo set bool "Show label"
+    LOOKUP: str = "lookup" # todo set link to lookup class 
+    LOOKUPARRAY: str = "lookupArray" # todo set lookup link
+    REFERENCE: str = "reference" # todo set domain link 
+    STRING: str = "string" # todo choose "Editor type" [default, password] lookup, "Max Length"
+    TEXT: str = "text" # todo choose "Type" [Plain text, Editor HTML, Editor MARKDOWN] lookup
+    TIME: str = "time" # todo set bool "Show second"
+    TIMESTAMP: str = "dateTime" # todo set bool "Show second"
+    RANGE: tuple = (BIGINTEGER, BOOLEAN, CHAR, DATE, DECIMAL, DOUBLE, FILE, FORMULA, INTEGER, IP_ADDRESS, \
         LINK, LOOKUP, LOOKUPARRAY, REFERENCE, STRING, TEXT, TIME, TIMESTAMP)
