4d887aba3c061ed8846cbed11dac2fd34bb743a8
boleque <vlkislitsyn1090@gmail.com>
2022-04-12 17:35:20 +0300

    Intro module improvements

--- apps/cmdbuild/scripts/__main__.py
+++ apps/cmdbuild/scripts/__main__.py
@@ -4,21 +4,31 @@ import argparse
 from requests import Session
 from scripts.version_updaters import update_version
 from scripts.web_clients.cmdbuild import CmdBuildWebClient
+from scripts.web_clients.hibob import HiBobWebClient
+
 
 if __name__ == '__main__':
 
     """
         Example:
-        python3 -m scripts --ip_address '127.0.0.1' --user 'admin' --pass 'admin' --cert_path '/home/vladislav/x-cmdbuild.key'
+        python3 -m scripts \
+            --ip_cmdbuild '127.0.0.1' \
+            --user_cmdbuild 'admin' \
+            --pass_cmdbuild 'admin' \
+            --cert_path_cmdbuild '~./x-cmdbuild.crt' \
+            --url_hibob 'https://api.hibob.com/v1/people?showInactive=false&includeHumanReadable=true',
+            --token_hibob '6RnF2ZjVjV'
     """
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--ip_address', dest='ip_address', type=str, required=True)
-    parser.add_argument('--user', dest='username', type=str, required=True)
-    parser.add_argument('--pass', dest='password', type=str, required=True)
-    parser.add_argument('--cert_path', dest='cert_path', type=str, required=True)
-    args = parser.parse_args()
+    parser.add_argument('--ip_cmdbuild', dest='ip_cmdbuild', type=str, required=True)
+    parser.add_argument('--user_cmdbuild', dest='user_cmdbuild', type=str, required=True)
+    parser.add_argument('--pass_cmdbuild', dest='pass_cmdbuild', type=str, required=True)
+    parser.add_argument('--url_hibob', dest='url_hibob', type=str, required=True)
+    parser.add_argument('--token_hibob', dest='token_hibob', type=str, required=True)
+    parser.add_argument('--cert_path_cmdbuild', dest='cert_path_cmdbuild', type=str)
 
+    args = parser.parse_args()
     logger: Logger = getLogger()
     logger.setLevel(INFO)
     handler: StreamHandler = StreamHandler(sys.stdout)
@@ -28,15 +38,15 @@ if __name__ == '__main__':
     logger.addHandler(handler)
 
     session: Session = Session()
-    session.verify = args.cert_path
-
-    base_url: str = f"https://{args.ip_address}/ready2use-2.2-3.4/services"
+    if args.cert_path_cmdbuild is not None:
+        session.verify = args.cert_path_cmdbuild
+    base_url: str = f"https://{args.ip_cmdbuild}/ready2use-2.2-3.4/services"
     cmdbuild_client: CmdBuildWebClient = CmdBuildWebClient(
         session=session,
         base_url=base_url,
         logger=logger
     )
     cmdbuild_client.path = "rest/v3/sessions?scope=service&returnId=true"
-    cmdbuild_client.generate_session_token(args.username, args.password)
-
-    update_version(cmdbuild_client, logger)
+    cmdbuild_client.generate_session_token(args.user_cmdbuildname, args.pass_cmdbuildword)
+    hibob_client: HiBobWebClient = HiBobWebClient(url=args.url_hibob, token=args.token_hibob)
+    update_version(hibob_client, cmdbuild_client, logger)
