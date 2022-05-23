9bcfc217e088c4d4f3eabb5949fa6841aa4fba32
boleque <vlkislitsyn1090@gmail.com>
2022-05-23 21:44:03 +0300

    Log path is added

--- apps/hr_system_scraper/app/main.py
+++ apps/hr_system_scraper/app/main.py
@@ -22,7 +22,7 @@ logger = logging.getLogger(APP_NAME)
 logger.setLevel(logging.INFO)
 formatter = logging.Formatter("%(asctime)s %(name)s %(levelname)s: %(message)s")
 ts = datetime.utcfromtimestamp(time.time()).strftime("%Y-%m-%d %H:%M:%S")
-file_logging = logging.FileHandler(f"{APP_NAME}-{ts}.log")
+file_logging = logging.FileHandler(f"logs/{APP_NAME}-{ts}.log")
 file_logging.setFormatter(formatter)
 logger.addHandler(file_logging)
 
